ebb860f247b34a6577e9704fc10835bf612b67a1
Sabeem101 <sabeemmouqere@gmail.com>
2024-03-20 19:15:35 -0700

    Minor update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or (roman_string is None):
-        return (0)
+        return 0
     rnum = {"I": 1, "V": 5, "X": 10, "L": 50,
             "C": 100, "D": 500, "M": 1000}
     rstr = roman_string
     con = [rnum[x[0]] if rnum[x[0]] >= rnum[x[1]] else (-1*rnum[x[0]])
             for x in zip(rstr, rstr[1:] + rstr[-1])]
-    return sum(con)
+    return (sum(con))

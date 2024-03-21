f09f41d7a1c6dfa403d041479b3b0b86f3dcbb24
Sabeem101 <sabeemmouqere@gmail.com>
2024-03-20 19:17:14 -0700

    Minor update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,5 @@ def roman_to_int(roman_string):
     rnum = {"I": 1, "V": 5, "X": 10, "L": 50,
             "C": 100, "D": 500, "M": 1000}
     rstr = roman_string
-    con = [rnum[x[0]] if rnum[x[0]] >= rnum[x[1]] else (-1*rnum[x[0]])
-        for x in zip(rstr, rstr[1:] + rstr[-1])]
+    con = [rnum[x[0]] if rnum[x[0]] >= rnum[x[1]] else (-1*rnum[x[0]]) for x in zip(rstr, rstr[1:] + rstr[-1])]
     return sum(con)

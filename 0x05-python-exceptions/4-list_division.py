52c6ea7cb92a6f6a6896cf2b3c1c4675136913c4
Sabeem101 <sabeemmouqere@gmail.com>
2024-04-02 06:25:05 -0700

    List division update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,9 +2,9 @@
 def list_division(my_list_1, my_list_2, list_length):
     arr = []
     res = 0
-    for x in range (0, list_length):
+    for el in range (0, list_length):
         try:
-            res = my_list_1[x]/my_list_2[x]
+            res = my_list_1[el] / my_list_2[el]
         except ZeroDivisionError:
             print("division by 0")
             res = 0

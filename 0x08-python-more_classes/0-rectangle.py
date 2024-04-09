70db676f99d418d690cfa94e9c6c8be6034f523c
Sabeem101 <sabeemmouqere@gmail.com>
2024-04-08 18:55:55 -0700

    Empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defining an empty rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Empty class that defines a rectangle.
+    Attributes:
+        Class doesn't have any attributes.
+    """
+    pass

e4f7c15d7b4131449a53dc73a4d962a6ab3cd0cb
Sabeem101 <sabeemmouqere@gmail.com>
2024-05-21 17:43:23 -0700

    Sorts scores

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- A script that lists the number of the records with the same score,
+-- in second_table of the database hbtn_0c_0 in MYSQL server.
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score
+ORDER BY number DESC;

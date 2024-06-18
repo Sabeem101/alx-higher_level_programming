028d347f388a3e6d02dcc10e50b0cdf025835743
Sabeem101 <sabeemmouqere@gmail.com>
2024-06-18 03:47:15 -0700

    Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Prints 'My number: <first argument converted in integer>.
+
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);

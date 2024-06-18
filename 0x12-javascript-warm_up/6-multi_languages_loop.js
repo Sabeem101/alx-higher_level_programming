f08075d3b00f00d1dfb8c2d80a20acff9e9f900c
Sabeem101 <sabeemmouqere@gmail.com>
2024-06-18 03:57:22 -0700

    Loop to languages - fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // Prints 3 lines by using an array of string and a loop.
 
 let x;
-const myString = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (x = 0; x < 3; x++) {
   console.log(myString[x]);
 }

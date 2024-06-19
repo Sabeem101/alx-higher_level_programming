176cd334e5e1dae29d5ffb018d5ef3deaaa6d8e8
Sabeem101 <sabeemmouqere@gmail.com>
2024-06-19 15:47:41 -0700

    Factor index - fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 /*
  * Imports an array and computes a new array.
  */
-const new_list = require('./100-data.js').list;
-console.log(new_list);
-console.log(new_list.map((item, index) => item * index));
+const nlist = require('./100-data.js').list;
+console.log(nlist);
+console.log(nlist.map((item, index) => item * index));

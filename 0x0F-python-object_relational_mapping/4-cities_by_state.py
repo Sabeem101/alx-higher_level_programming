4dfdb508068d51866bf9ee237c8bbaf8cb516e3f
Sabeem101 <sabeemmouqere@gmail.com>
2024-06-21 08:14:11 -0700

    Lists cities by state - fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,18 +14,18 @@ if __name__ == '__main__':
             host="localhost", user=argv[1], port=3306,
             passwd=argv[2], db=argv[3])
 
-    with DB.cursor() as crsr:
-        crsr.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
+    crsr = DB.cursor()
+    crsr.execute("""
+        SELECT
+            cities.id, cities.name, states.name
+        FROM
+            cities
+        JOIN
+            states
+        ON
+            cities.state_id = states.id
+        ORDER BY
+            cities.id ASC
         """)
     rows = crsr.fetchall()
 
